b6a0fee62a9f86878e599a330de7cb94e601afdf
SamarDrame <sdrame@leseminaire.ca>
2024-11-30 20:34:49 +0000

    Premiere modif

--- game.py
+++ game.py
@@ -1,1 +1,14 @@
+import random
+def roulerlesdes(face):
+   valeur = random.randint(1,face)
+   return valeur
 
+question1 = str(input("Voulez-vous rouler les dès?"))
+question2 = int(input("Combien de face voulez-vous?"))
+
+
+if question1 == "oui":
+   patate = roulerlesdes(question2)
+   print(patate)
+   if patate == question2:
+      print(gagné)
