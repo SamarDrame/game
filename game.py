763f0fd9bd24373848112dfd82432b71f98e797a
SamarDrame <sdrame@leseminaire.ca>
2024-12-02 17:20:58 +0000

    Message

--- game.py
+++ game.py
@@ -4,11 +4,16 @@ def roulerlesdes(face):
    return valeur
 
 question1 = str(input("Voulez-vous rouler les dès?"))
-question2 = int(input("Combien de face voulez-vous?"))
-
 
 if question1 == "oui":
-   patate = roulerlesdes(question2)
-   print(patate)
-   if patate == question2:
-      print(gagné)
+   question2 = int(input("Combien de face voulez-vous?"))
+   de = roulerlesdes(question2)
+   print(de)
+   if de == question2:
+      print("gagné")
+   else: 
+      print ("perdu")
+
+
+if question1 == "non": 
+   print ("vous ne voulez pas jouer")
